87af2cb0a92327ea0ced5784ecb3e7fc7cd99eaa
Nhat Hao <nhathao230803@gmail.com>
2024-06-17 12:59:35 +0700

    [feat] Add_Template

---
+++ PRN221.ClinicDental.Presentation/Pages/Index123.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN221.ClinicDental.Presentation.Pages
+{
+    public class Index123Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
