a6f1ee5e269a5e60e990be1228e8694ce62dc54a
Nguyễn Trung Hậu <haunt150603@gmail.com>
2024-06-26 22:11:56 +0700

    delete

--- PRN221.ClinicDental.Data/Models/Clinic.cs
+++ PRN221.ClinicDental.Data/Models/Clinic.cs
@@ -18,9 +18,6 @@ public partial class Clinic
 
     public int ClinicOwnerId { get; set; }
 
-    public TimeOnly OpeningTime { get; set; }
-
-    public TimeOnly ClosingTime { get; set; }
 
     [ForeignKey("AddressId")]
     [InverseProperty("Clinic")]
