7e5d5c436cde69292dc991d98b3d54865ed99d27
kien <kien.nguyentrung1699@gmail.com>
2024-07-18 12:13:17 +0700

    fix updateProfile

--- PRN221.ClinicDental.Business/DTO/Request/UserProfileResponse.cs
+++ PRN221.ClinicDental.Business/DTO/Request/UserProfileResponse.cs
@@ -8,12 +8,12 @@ namespace PRN221.ClinicDental.Business.DTO.Request
 {
     public class UserProfileResponse
     {
-        public int UserId { get; set; }
-        public string Username { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
+        public int? UserId { get; set; }
+        public string? Username { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Address { get; set; }
-        public int RoleId { get; set; }
+        public int? RoleId { get; set; }
     }
 }
